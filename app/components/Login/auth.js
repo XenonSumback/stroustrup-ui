0c4bfd1eccc58b79140f5578da59beec5d1d3052
Marina M <xenonsumback@gmail.com>
2017-05-10 22:46:07 +0600

    fix auth and login file

---
+++ app/components/Login/auth.js
@@ -0,0 +1,27 @@
+module.exports = {
+    login(username, pass) {
+        var userInfo = {
+            username: username,
+            password: pass
+        }
+        var data = module.exports.getFormData(userInfo)
+        fetch('http://127.0.0.1:8000/login/',{
+            method: 'POST',
+            mode:'no-cors',
+            credentials: "include",
+            /*headers: {
+              'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+              'Content-Type': 'multipart/form-data; charset=utf-8'
+            },*/
+            body: data
+          }).then((response) => {
+            console.log(response.body)
+            response.json()
+          })
+        },
+    getFormData(object) {
+        const formData = new FormData();
+        Object.keys(object).forEach(key => formData.append(key, object[key]));
+        return formData;
+      }
+}
