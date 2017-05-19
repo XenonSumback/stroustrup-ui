49c64eea8dc050db4858a56497fcb5d6d6d3a458
Marina M <xenonsumback@gmail.com>
2017-05-19 21:56:53 +0600

    added registration module(not worked yet)

---
+++ app/components/Registration/Untitled Folder/index.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react'
+import Input from './Input'
+
+
+class Registration extends Component {
+
+  getInitialState () {
+    return {
+      email: null,
+      password: null,
+      confirmPassword: null,
+      forbiddenWords: ["password", "user", "username"],
+    }
+  }
+
+  handlePasswordInput = (event) => {
+    if(!_.isEmpty(this.state.confirmPassword)){
+      this.refs.passwordConfirm.isValid();
+    }
+    this.refs.passwordConfirm.hideError();
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  handleConfirmPasswordInput = (event) => {
+    this.setState({
+      confirmPassword: event.target.value
+    });
+  }
+
+  saveAndContinue = (event) => {
+    e.preventDefault();
+
+    var canProceed = this.validateEmail(this.state.email)
+        && this.refs.password.isValid()
+        && this.refs.passwordConfirm.isValid();
+
+    if(canProceed) {
+      var data = {
+        email: this.state.email,
+      }
+      alert('Thanks.');
+    } else {
+      this.refs.email.isValid();
+      this.refs.password.isValid();
+      this.refs.passwordConfirm.isValid();
+    }
+  }
+
+  isConfirmedPassword = (event) => {
+    return (event == this.state.password)
+  }
+
+
+  handleEmailInput = (event) =>{
+    this.setState({
+      email: event.target.value
+    });
+  }
+
+  validateEmail = (event) => {
+    // regex from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(event);
+  }
+
+  isEmpty (value) {
+    return !_.isEmpty(value);
+  }
+
+
+  render() {
+    return (
+      <div className="create_account_screen">
+
+        <div className="create_account_form">
+          <h1>Create account</h1>
+          <p>Example of form validation built with React.</p>
+          <form onSubmit={this.saveAndContinue}>
+
+            <Input
+              text="Email Address"
+              ref="email"
+              type="text"
+              defaultValue={this.state.email}
+              validate={this.validateEmail}
+              value={this.state.email}
+              onChange={this.handleEmailInput}
+              errorMessage="Email is invalid"
+              emptyMessage="Email can't be empty"
+              errorVisible={this.state.showEmailError}
+            />
+
+            <Input
+              text="Password"
+              type="password"
+              ref="password"
+              validator="true"
+              minCharacters="8"
+              requireCapitals="1"
+              requireNumbers="1"
+              forbiddenWords={this.state.forbiddenWords}
+              value={this.state.passsword}
+              emptyMessage="Password is invalid"
+              onChange={this.handlePasswordInput}
+            />
+
+            <Input
+              text="Confirm password"
+              ref="passwordConfirm"
+              type="password"
+              validate={this.isConfirmedPassword}
+              value={this.state.confirmPassword}
+              onChange={this.handleConfirmPasswordInput}
+              emptyMessage="Please confirm your password"
+              errorMessage="Passwords don't match"
+            />
+
+            <button
+              type="submit"
+              className="button button_wide">
+              CREATE ACCOUNT
+            </button>
+
+          </form>
+
+           <a href="https://github.com/mikepro4/react-signup-form" target="_blank" className="github_link" title="View Source Code">
+              <Icon type="guthub" />
+          </a>
+        </div>
+
+      </div>
+    );
+  }
+
+}
+export default Registration
