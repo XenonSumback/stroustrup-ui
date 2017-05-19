9e98ddb5699c35ed599052194ec43f2e388b5560
Marina M <xenonsumback@gmail.com>
2017-05-19 16:33:51 +0600

    add onClick handler, for BookList(in progress)

--- app/components/BookList/BookList.js
+++ app/components/BookList/BookList.js
@@ -5,12 +5,24 @@ import Book from '../Book'
 
 class BookList extends Component {
 
+  constructor(props) {
+      super(props);
+      this.state = {};
+
+      // This binding is necessary to make `this` work in the callback
+      this.handleClick = this.handleClick.bind(this);
+    }
+
+  handleClick = (e) => {
+     e.preventDefault()
+    console.log('this is:',this,  e.target);
+  }
    render(){
        return(
           <div>
-          <h3>BookList</h3><br/>
+          <h3>BookList</h3>
           {this.props.books.map((book, i) =>
-          <li key={i}><a href='/{book.id}'>{book.name_book}</a></li>
+          <li key={i} ><a key={book} onClick={(e) => this.handleClick(e)}>{book.name_book}</a></li>
         )}</div>
        );
    }
