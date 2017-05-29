bf248e3513453621b78ba6911eecce903811cddf
Marina M <xenonsumback@gmail.com>
2017-05-29 16:41:17 +0600

    Add comments creating component

--- app/components/Comments/CreateComment.js
+++ app/components/Comments/CreateComment.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react'
 import { connect } from 'react-refetch'
-import Comment from './Comment'
+
 
 
 
 export class CreateComment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''}
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value})
+  }
+
+  handleClick (e) {
+    this.props.postComment(comment.value)
+  }
 
    render(){
      const id = this.props.id
+     var value = this.state.value
              return (
              <div className="panel panel-default">
                <div className="panel-heading">
                  <h3 className="panel-title"></h3>
                </div>
                <div className="panel-body">
-                <input type="text" className="form-control" placeholder="Comment" ref="comment" />
+                <input type="text" className="form-control" id="comment" placeholder="Comment"
+                 ref="comment" value={value} onChange={this.handleChange} />
                </div>
                <div className="panel-footer">
-                <input  type="submit" className="form-control" onClick={() => {this.props.postComment()}} />
+                <input  type="submit" className="form-control" onClick={(e) =>this.handleClick(e)} />
                </div>
              </div>
-
             )
     }
 }
@@ -28,20 +43,19 @@ export class CreateComment extends Component {
 export default connect(props => {
   const id = props.id
   const url = 'http://127.0.0.1:8000/books/'+id+'/comments/'
-  const date = new Date().toISOString().slice(0,10);
+  const date = new Date().toISOString().slice(0,10)
   const user = 'user'
   return{
-  postComment: () => ({
+  postComment: (comment) => ({
     postCommentResponse: {
       url: url,
       method: 'POST',
       credentials: 'include',
       mode: 'no-cors',
       body: JSON.stringify({
-        "user_id": "1",
-        "book_id": id,
+        "user": 1,
         "date": date,
-        "comment": "comt book(request)",
+        "comment": comment,
        })
     }
   })
