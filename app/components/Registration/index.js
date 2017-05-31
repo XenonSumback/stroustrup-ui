11f2647aa7d03d3c3ba6b1b010f71b5330fab426
Marina M <xenonsumback@gmail.com>
2017-05-31 18:08:25 +0600

    Add form for registration.

--- app/components/Registration/index.js
+++ app/components/Registration/index.js
@@ -1,178 +1,87 @@
-var React = require('react');
-var Input = require('./components/Input.js');
-var _ = require('underscore');
-var Select = require('./components/Select');
-var STATES = require('./components/data/states');
-var Icon = require('./components/Icon.js');
+import React, { Component } from 'react'
+import Formsy from 'formsy-react';
+import { connect } from 'react-refetch'
+import MyInput from './MyInput'
 
-var CreateAccountScreen = React.createClass({
-  getInitialState: function () {
-    return {
-      email: null,
-      companyName: null,
-      password: null,
-      confirmPassword: null,
-      statesValue: null,
-      forbiddenWords: ["password", "user", "username"]
-    }
-  },
+export class Registration extends Component {
 
-  handlePasswordInput: function (event) {
-    if(!_.isEmpty(this.state.confirmPassword)){
-      this.refs.passwordConfirm.isValid();
-    }
-    this.refs.passwordConfirm.hideError();
-    this.setState({
-      password: event.target.value
-    });
-  },
-
-  handleConfirmPasswordInput: function (event) {
-    this.setState({
-      confirmPassword: event.target.value
-    });
-  },
-
-  saveAndContinue: function (e) {
-    e.preventDefault();
-
-    var canProceed = this.validateEmail(this.state.email)
-        && !_.isEmpty(this.state.statesValue)
-        && this.refs.password.isValid()
-        && this.refs.passwordConfirm.isValid();
-
-    if(canProceed) {
-      var data = {
-        email: this.state.email,
-        state: this.state.statesValue
+  constructor(props) {
+      super(props);
+      this.state = {
+      canSubmit: false
       }
-      alert('Thanks.');
-    } else {
-      this.refs.email.isValid();
-      this.refs.state.isValid();
-      this.refs.companyName.isValid();
-      this.refs.password.isValid();
-      this.refs.passwordConfirm.isValid();
+      this.enableButton = this.enableButton.bind(this)
+      this.disableButton = this.disableButton.bind(this)
+
     }
-  },
 
-  isConfirmedPassword: function (event) {
-    return (event == this.state.password)
-  },
-
-  handleCompanyInput: function(event) {
-    this.setState({
-      companyName: event.target.value
-    })
-  },
-
-  handleEmailInput: function(event){
-    this.setState({
-      email: event.target.value
-    });
-  },
-
-  validateEmail: function (event) {
-    // regex from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(event);
-  },
-
-  isEmpty: function (value) {
-    return !_.isEmpty(value);
-  },
-
-  updateStatesValue: function (value) {
-    this.setState({
-      statesValue: value
-    })
-  },
-
-  render: function() {
-    return (
-      <div className="create_account_screen">
-
-        <div className="create_account_form">
-          <h1>Create account</h1>
-          <p>Example of form validation built with React.</p>
-          <form onSubmit={this.saveAndContinue}>
-
-            <Input
-              text="Email Address"
-              ref="email"
-              type="text"
-              defaultValue={this.state.email}
-              validate={this.validateEmail}
-              value={this.state.email}
-              onChange={this.handleEmailInput}
-              errorMessage="Email is invalid"
-              emptyMessage="Email can't be empty"
-              errorVisible={this.state.showEmailError}
-            />
-
-            <Input
-              text="Company Name"
-              ref="companyName"
-              validate={this.isEmpty}
-              value={this.state.companyName}
-              onChange={this.handleCompanyInput}
-              emptyMessage="Company name can't be empty"
-            />
-
-            <Input
-              text="Password"
-              type="password"
-              ref="password"
-              validator="true"
-              minCharacters="8"
-              requireCapitals="1"
-              requireNumbers="1"
-              forbiddenWords={this.state.forbiddenWords}
-              value={this.state.passsword}
-              emptyMessage="Password is invalid"
-              onChange={this.handlePasswordInput}
-            />
-
-            <Input
-              text="Confirm password"
-              ref="passwordConfirm"
-              type="password"
-              validate={this.isConfirmedPassword}
-              value={this.state.confirmPassword}
-              onChange={this.handleConfirmPasswordInput}
-              emptyMessage="Please confirm your password"
-              errorMessage="Passwords don't match"
-            />
-
-            <Select
-              options={STATES}
-              ref="state"
-              value={this.state.statesValue}
-              onChange={this.updateStatesValue}
-              searchable={this.props.searchable}
-              emptyMessage="Please select state"
-              errorMessage="Please select state"
-              placeholder="Choose Your State"
-              placeholderTitle="Your State"
-            />
-
-            <button
-              type="submit"
-              className="button button_wide">
-              CREATE ACCOUNT
-            </button>
-
-          </form>
-
-           <a href="https://github.com/mikepro4/react-signup-form" target="_blank" className="github_link" title="View Source Code">
-              <Icon type="guthub" />
-          </a>
-        </div>
-
-      </div>
-    );
+  submit(data) {
+    alert(JSON.stringify(data, null, 4))
   }
+  enableButton() {
+    this.setState({ canSubmit: true })
 
-});
+  }
+  disableButton() {
+    this.setState({ canSubmit: false })
+  }
+    render() {
+      const postUser = this.props
+      return (
+        <div className="container">
+          <Formsy.Form onSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton} className="login">
+          <div className="row">
+            <div className="col-md-4 col-md-offset-4">
+              <div className="form-group">
+                <label htmlFor="InputUsername">Username</label>
+                <MyInput className="form-control" name="username" id="InputUsername" required />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4 col-md-offset-4">
+              <div className="form-group">
+                <label htmlFor="InputEmail">Email</label>
+                <MyInput className="form-control" name="email" id="InputEmail" validations="isEmail" validationError="This is not a valid email" required />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4 col-md-offset-4">
+              <div className="form-group">
+                <label htmlFor="InputPassword">Password</label>
+                <MyInput className="form-control" name="password" id="InputPassword" type="password" required />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4 col-md-offset-4">
+              <div className="form-group">
+                <label htmlFor="ConfirmPassword">ConfirmPassword</label>
+                <MyInput className="form-control" name="confirmpassword" id="ConfirmPassword" type="password" validations="equalsField:password" validationError="Passwords not match" required />
+              </div>
+            </div>
+          </div>
+          <div className="buttons">
+           <button type="submit" className="form-control" disabled={!this.state.canSubmit}>Submit</button>
+          </div>
 
-module.exports = CreateAccountScreen;
+           </Formsy.Form>
+        </div>
+      );
+    }
+}
+export default connect(props => {
+  const url = 'http://127.0.0.1:8000/users/'
+  return{
+  postUser: data => ({
+    postUserResponse: {
+      url: url,
+      method: 'POST',
+      credentials: 'include',
+      mode: 'no-cors',
+      body: JSON.stringify({ data })
+    }
+  })
+}
+})(Registration)
--- app/containers/RegistrationPage/index.js
+++ app/containers/RegistrationPage/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import CreateAccountScreen from '../../components/Registration/';
+import Registration from '../../components/Registration/';
 
 export default class RegistrationPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
   render() {
     return (
-      <CreateAccountScreen />
+      <Registration />
     );
   }
 }
